43be7b27008bdabf47161c69531e64582154e4f8
Phant0m The Great <120599821+Phant0mthegreat@users.noreply.github.com>
2023-04-23 16:11:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ while True:
         if len(cep2)==8:
             url2='https://viacep.com.br/ws/{}/json/'.format(cep2)
             res2 = requests.get(url2);req2=res2.json()
-            resl2="\n{}-------------------------------------------------\nby: Phant0m The Great{} \n{}[Cep]:{}\n[Logradouro]:{}\n[Complemento]:{}\n[Bairro]:{}\n[Localidade]:{}\n[Uf]:{}\n[Ibge]:{}\n[Gia]:{} {}\n-------------------------------------------------".format(ng,f,green,req2['cep'],req2['logradouro'],req2['complemento'],req2['bairro'],req2['localidade'],req2['uf'],req2['ibge'],req2['gia'],f)        
+            resl2="\n{}-------------------------------------------------\n{}{}[Cep]:{}\n[Logradouro]:{}\n[Complemento]:{}\n[Bairro]:{}\n[Localidade]:{}\n[Uf]:{}\n[Ibge]:{}\n[Gia]:{} {}\n-------------------------------------------------".format(ng,f,green,req2['cep'],req2['logradouro'],req2['complemento'],req2['bairro'],req2['localidade'],req2['uf'],req2['ibge'],req2['gia'],f)        
             print(resl2)
             input('[ENTER] para voltar ao menu.')
         else:
@@ -102,8 +102,9 @@ while True:
           
             url3='https://ws.apicep.com/cep/{}.json'.format(cep3) 
             res3 = requests.get(url3);req3=res3.json()
-            resl3="\n{}-------------------------------------------------\nby: Phant0m The Great{} \n{}[Status]:{}\n[Ok]:{}\n[Código]:{}\n[Estado]:{}\n[Cidade]:{}\n[Distrito]:{}\n[Endereço]:{}\n[StatusText]:{} {}\n-------------------------------------------------".format(ng,f,green,req3['status'],req3['ok'],req3['code'],req3['state'],req3['city'],req3['district'],req3['address'],req3['statusText'],f)
+            resl3="\n{}-------------------------------------------------\n{}{}[Status]:{}\n[Ok]:{}\n[Código]:{}\n[Estado]:{}\n[Cidade]:{}\n[Distrito]:{}\n[Endereço]:{}\n[StatusText]:{} {}\n-------------------------------------------------".format(ng,f,green,req3['status'],req3['ok'],req3['code'],req3['state'],req3['city'],req3['district'],req3['address'],req3['statusText'],f)
             print(resl3)
+            input('[ENTER] para voltar ao menu.')
         else:
             print(f' {vm}[!] valor invalido \n digite nesse formato \n {f}ex: {ng}00000000 {f}')
             print('')
@@ -126,7 +127,7 @@ while True:
       
       url1='https://www.receitaws.com.br/v1/cnpj/{}'.format(cpnj1)
       res=requests.get(url1);req1=res.json()
-      br="{}\n-----------------------------------------------by: Phant0m The Great {}\n{}[Data situação]:{}\n[Motivo situação]:{}\n[Complemento]:{}\n[Tipo]:{}\n[Nome]:{}\n[Telefone]:{}\n[Situação]:{}\n[Bairro]:{}\n[Logradouro]:{}\n[Número]:{}\n[Cep]:{}\n[Municipio]:{}\n[Fantasia]:{}\n[Porte]:{}\n[Abertura]:{}\n[Natureza juridica]:{}\n[Uf]:{}\n[Cnpj]:{}\n[Ultima atualização]:{}\n[Status]:{}\n[Email]:{}\n[Efr]:{}\n[Situação especial]:{}\n[Data situação especial]:{}".format(ng,f,green,req1['data_situacao'],req1['motivo_situacao'],req1['complemento'],req1['tipo'],req1['nome'],req1['telefone'],req1['situacao'],req1['bairro'],req1['logradouro'],req1['numero'],req1['cep'],req1['municipio'],req1['fantasia'],req1['porte'],req1['abertura'],req1['natureza_juridica'],req1['uf'],req1['cnpj'],req1['ultima_atualizacao'],req1['status'],req1['email'],req1['efr'],req1['situacao_especial'],req1['data_situacao_especial'],f)
+      br="{}\n-----------------------------------------------{}{}[Data situação]:{}\n[Motivo situação]:{}\n[Complemento]:{}\n[Tipo]:{}\n[Nome]:{}\n[Telefone]:{}\n[Situação]:{}\n[Bairro]:{}\n[Logradouro]:{}\n[Número]:{}\n[Cep]:{}\n[Municipio]:{}\n[Fantasia]:{}\n[Porte]:{}\n[Abertura]:{}\n[Natureza juridica]:{}\n[Uf]:{}\n[Cnpj]:{}\n[Ultima atualização]:{}\n[Status]:{}\n[Email]:{}\n[Efr]:{}\n[Situação especial]:{}\n[Data situação especial]:{}".format(ng,f,green,req1['data_situacao'],req1['motivo_situacao'],req1['complemento'],req1['tipo'],req1['nome'],req1['telefone'],req1['situacao'],req1['bairro'],req1['logradouro'],req1['numero'],req1['cep'],req1['municipio'],req1['fantasia'],req1['porte'],req1['abertura'],req1['natureza_juridica'],req1['uf'],req1['cnpj'],req1['ultima_atualizacao'],req1['status'],req1['email'],req1['efr'],req1['situacao_especial'],req1['data_situacao_especial'],f)
       print(br)
       print(f'''{f}-''' * 47)
       input('[ENTER] para voltar ao menu.')
@@ -161,7 +162,7 @@ while True:
       url2='http://ip-api.com/json/{}'.format(ip2)
       res2 = requests.get(url2)
       req1=res2.json()
-      br2="\n{}-------------------------------------------------\n by: Phant0m The Great{}\n {}[Status]:{}\n [País]:{}\n [Código do país]:{}\n [Região]:{}\n [Nome da região]:{}\n [Cidade]:{}\n [Zip]:{}\n [Lat]:{}\n [Lon]:{}\n [Fuso horário]:{}\n [Isp]:{}\n [Org]:{}\n [As]:{} {}\033[m\n-------------------------------------------------".format(ng,f,az,req1['status'],req1['country'],req1['countryCode'],req1['region'],req1['regionName'],req1['city'],req1['zip'],req1['lat'],req1['lon'],req1['timezone'],req1['isp'],req1['org'],req1['as'],req1['query'],f)
+      br2="\n{}-------------------------------------------------\n{}{}[Status]:{}\n [País]:{}\n [Código do país]:{}\n [Região]:{}\n [Nome da região]:{}\n [Cidade]:{}\n [Zip]:{}\n [Lat]:{}\n [Lon]:{}\n [Fuso horário]:{}\n [Isp]:{}\n [Org]:{}\n [As]:{} {}\033[m\n-------------------------------------------------".format(ng,f,az,req1['status'],req1['country'],req1['countryCode'],req1['region'],req1['regionName'],req1['city'],req1['zip'],req1['lat'],req1['lon'],req1['timezone'],req1['isp'],req1['org'],req1['as'],req1['query'],f)
       print(br2)
       input('[ENTER] para voltar ao menu.')
     elif n1=='2' or n1=='02':
@@ -182,7 +183,7 @@ while True:
       url1 = "https://api.ipgeolocation.io/ipgeo?apiKey=9313e7887bad45cea6d4b5845f085464&ip={}".format(ip1)
       res = requests.get(url1)
       req=res.json()
-      br="\n{}-------------------------------------------------\nby: Phant0m The Great{}{} \n[Ip]:{}\n[Código do continente]:{}\n[Nome do continente]:{}\n[Código do país 2]:{}\n[Código do país 3]:{}\n[Nome do país]:{}\n[Capital do país]:{}\n[Prov do estado]:{}\n[Distrito]:{}\n[Cidade]:{}\n[CEP]:{}\n[Latitude]:{}\n[Longitude]:{}\n[Is_eu]:{}\n[Código de chamada]:{}\n[País tld]:{}\n[Languages]:{}".format(ng,f,az,req['ip'],req['continent_code'],req['continent_name'],req['country_code2'],req['country_code3'],req['country_name'],req['country_capital'],req['state_prov'],req['district'],req['city'],req['zipcode'],req['latitude'],req['longitude'],req['is_eu'],req['calling_code'],req['country_tld'],req['languages'],req['country_flag'],req['geoname_id'])
+      br="\n{}-------------------------------------------------\n{}{} \n[Ip]:{}\n[Código do continente]:{}\n[Nome do continente]:{}\n[Código do país 2]:{}\n[Código do país 3]:{}\n[Nome do país]:{}\n[Capital do país]:{}\n[Prov do estado]:{}\n[Distrito]:{}\n[Cidade]:{}\n[CEP]:{}\n[Latitude]:{}\n[Longitude]:{}\n[Is_eu]:{}\n[Código de chamada]:{}\n[País tld]:{}\n[Languages]:{}".format(ng,f,az,req['ip'],req['continent_code'],req['continent_name'],req['country_code2'],req['country_code3'],req['country_name'],req['country_capital'],req['state_prov'],req['district'],req['city'],req['zipcode'],req['latitude'],req['longitude'],req['is_eu'],req['calling_code'],req['country_tld'],req['languages'],req['country_flag'],req['geoname_id'])
       print(br)
       input('[ENTER] para voltar ao menu.')
   elif esc=='04' or esc=='4':
